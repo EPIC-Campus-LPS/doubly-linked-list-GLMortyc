9819aabaa3f81602db0b16d6ae3c23ad245efce2
Morgan Chan <99379848+GLMortyc@users.noreply.github.com>
2026-04-13 12:17:01 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+{
+    public static void main(String[] args) {
+        Node<String> first = new Node<>("obtuse");
+
+        Node<String> next = new Node<>("rubber goose");
+        first.setNextNode(next);
+        next.setPrevNode(first);
+
+        Node<String> after = new Node<>("guava juice", next);
+        next.setNextNode(after);
+        System.out.println(after.getPrevNode().getValue()); // "rubber goose"
+
+        Node<String> last = new Node<>("giant snake");
+        System.out.println(next.equals(last));
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list.toString()); // [10, 20, 30]
+
+        list.add(2, 40);
+        list.add(0, 5);
+        list.set(2, 50);
+        System.out.println(list.toString()); // [5, 10, 50, 40, 30]
+
+        list.remove(4);
+        list.remove(2);
+        System.out.println(list.size()); // 3
+        System.out.println(list.toString()); // [5, 10, 40]
+    }
+}
