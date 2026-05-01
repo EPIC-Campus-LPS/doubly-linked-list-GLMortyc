0403749344e20262d74470eddff6311e5c9f042b
Morgan Chan <99379848+GLMortyc@users.noreply.github.com>
2026-05-01 15:11:21 -0600

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,58 +1,73 @@
 public class DoublyLinkedList<E> implements List {
-    private E value;
     private Node<E> head;
     private Node<E> last;
-    Object [] list = new Object[0];
-
-    public void add(Object element) {
-        Node<E> a = new Node<E>(element);
+    private int count = 0;
+    
+    public void add(Object e) {
+        count += 1;
+        Node<E> a = new Node(e);
         if (head == null) {
-            head = last = Node<E> ;
+            head = last = a;
         } else {
             Node<E> temp = last;
-            last = (Node<E>) element;
+            last = a;
             temp.setNextNode(last);
         }
     }
 
-    public void add(int p, Object element) throws IndexOutOfBoundsException {
-        Node<E> a = (Node<E>) element;
-
-
+    public void add(int p, Object e) throws IndexOutOfBoundsException {
+        count += 1;
+        Node<E> a = new Node(e);
+        Node<E> temp = last;
+        last = a;
+        temp.setNextNode(last);
     }
 
     public Object remove() {
+        count -= 1;
         Node<E> temp = last;
 
         return temp;
     }
 
     public Object remove(int i) throws IndexOutOfBoundsException {
-        return null;
+        count -= 1;
+        Node<E> temp = last;
+        temp.toString();
+        return temp;
     }
 
     public Node<E> get(int p) throws IndexOutOfBoundsException {
-        return (Node<E>) list[p];
+        Node<E> g = new Node(p);
+        g.toString();
+        return g;
+
     }
 
-    public void set(int i, Object element) throws IndexOutOfBoundsException {
-        list[i]= (Node<E>) element;
+    public void set(int i, Object e) throws IndexOutOfBoundsException {
+        Node<E> a = new Node(e);
+        last = a;
     }
 
 
     public int size() {
-        return list.length;
+        return count;
     }
 
 
     public boolean isEmpty() {
-        if (list.length == 0) {
+        if (count == 0) {
             return true;
         }
         return false;
     }
 
     public String toString() {
-        return list.toString();
+        String output = head.toString() + ", ";
+        for(int i = 1; i < count - 1; i++) {
+            output += last.toString().toString() + ", ";
+        }
+        output += last.toString();
+        return output;
     }
 }
